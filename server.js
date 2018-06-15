16ba4ed8f2b65fe3d020af7e97135c5eebd3d6eb
codeunifier <eva15023@byui.edu>
2018-06-14 17:40:18 -0700

    added server timeout

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ function HandleRequests(request, response) {
     }
 }
 
-if(http.createServer(HandleRequests).listen(port)) {
+var server = http.createServer(HandleRequests).listen(port);
+if(server) {
     isStarted = true;
+    server.timeout = 1000;
+    
     console.log("Server connected at port " + port);
 }
