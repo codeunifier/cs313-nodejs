d4cdce55d83d54496e7e3cd6a7f3a099525cf489
codeunifier <eva15023@byui.edu>
2018-06-24 10:04:09 -0700

    replaced mongoose crap

--- local_modules/mon-mid.js
+++ local_modules/mon-mid.js
@@ -10,57 +10,57 @@ const MONGODB_URI = process.env.MONGOLAB_URI;
 // const MONGODB_URI = 'mongodb://localhost:27977/planechat';
 
 module.exports.attemptConnection = function attemptConnection(callback) {
-    // mongoose.connect(MONGODB_URI, {
-    //     connectTimeoutMS: 10000,
-    //     socketTimeoutMS: 10000
-    //   }).catch(err => {
-    //     callback(err);
-    //   });
+    mongoose.connect(MONGODB_URI, {
+        connectTimeoutMS: 10000,
+        socketTimeoutMS: 10000
+      }).catch(err => {
+        callback(err);
+      });
 }
 
 module.exports.insertNewUser = function insertNewUser(model, callback) {
-    // var sanUser = sanitize(model.username);
-    // var sanPass = sanitize(model.password);
+    var sanUser = sanitize(model.username);
+    var sanPass = sanitize(model.password);
 
-    // bcrypt.hash(sanPass, SALT_ROUNDS, function (err, hash) {
-    //     if (err) {
-    //         callback(err, false);
-    //     } else {
-    //         var dbModel = {
-    //             username: sanUser,
-    //             password_hash: hash,
-    //             date_created: new Date()
-    //         };
+    bcrypt.hash(sanPass, SALT_ROUNDS, function (err, hash) {
+        if (err) {
+            callback(err, false);
+        } else {
+            var dbModel = {
+                username: sanUser,
+                password_hash: hash,
+                date_created: new Date()
+            };
 
-    //         mongoose.connection.collection("users").insert(dbModel, function (err, doc) {
-    //             if (err) {
-    //                 callback(err, false);
-    //             } else {
-    //                 callback(doc, true);
-    //             }
-    //         });
-    //     }
-    // });
+            mongoose.connection.collection("users").insert(dbModel, function (err, doc) {
+                if (err) {
+                    callback(err, false);
+                } else {
+                    callback(doc, true);
+                }
+            });
+        }
+    });
 }
 
 module.exports.validateUser = function validateUser(username, pass, callback) {
-    // var sanUser = sanitize(username);
-    // var sanPass = sanitize(pass);
+    var sanUser = sanitize(username);
+    var sanPass = sanitize(pass);
 
-    // User.findOne({ username: sanUser }, function (err, user) {
-    //     if (err) {
-    //       callback(err, null);
-    //     } else {
-    //       if (user == null) {
-    //         //no user found in database with that username
-    //         callback(null, false);
-    //       } else {
-    //         user = user.toObject();
+    User.findOne({ username: sanUser }, function (err, user) {
+        if (err) {
+          callback(err, null);
+        } else {
+          if (user == null) {
+            //no user found in database with that username
+            callback(null, false);
+          } else {
+            user = user.toObject();
             
-    //         bcrypt.compare(sanPass, user.password_hash).then(function (res) {
-    //             callback(null, res);
-    //         });
-    //       }
-    //     };
-    // });
+            bcrypt.compare(sanPass, user.password_hash).then(function (res) {
+                callback(null, res);
+            });
+          }
+        };
+    });
 }
